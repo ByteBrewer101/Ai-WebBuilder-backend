d6bc4e70cdb0c5274cdd2fdce01607af95e6036e
Atharva <161151563+ByteBrewer101@users.noreply.github.com>
2025-09-15 09:54:20 +0530

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   userId: { type: String, unique: true }, 
   date: { type: Date, default: Date.now }, 
-  attemptsLeft: { type: Number, default: 30000 }, 
+  attemptsLeft: { type: Number, default: 3 }, 
 });
 
 export const userModel = mongoose.model("userModel", userSchema);
